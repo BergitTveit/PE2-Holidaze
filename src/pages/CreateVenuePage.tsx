23195afbf73e1235fa8b8fa909639256de45f663
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-01-31 16:05:20 +0100

    Create venue Page

---
+++ src/pages/CreateVenuePage.tsx
@@ -0,0 +1,12 @@
+import { AddVenueForm } from '../components/venues/addVenueForm';
+
+const CreateVenuePage = () => {
+  return (
+    <div className="max-w-3xl mx-auto py-8 px-4">
+      <h1 className="text-2xl font-bold mb-6">Create New Venue</h1>
+      <AddVenueForm />
+    </div>
+  );
+};
+
+export default CreateVenuePage;
