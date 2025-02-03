ebd2464d7cc8260519800b0939d98bea4fdcc5d2
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-03 10:35:20 +0100

    Reinstate location schema with only the desired fields

--- src/schemas/addVenue.ts
+++ src/schemas/addVenue.ts
@@ -1,26 +1,12 @@
-// import * as z from 'zod';
-
-// export const addVenueSchema = z.object({
-//   name: z.string().min(1, 'Required'),
-//   description: z.string().min(1, 'Required'),
-//   price: z.number().min(1, 'Price must be at least 1'),
-//   maxGuests: z.number().min(1, 'Must have at least 1 maximum guest'),
-// });
-
-// export type CreateVenueDTO = z.infer<typeof addVenueSchema>;
-
 import * as z from 'zod';
 import { mediaSchema } from './shared/media';
 
-// const locationSchema = z.object({
-//   address: z.string().min(1, 'Address is required'),
-//   city: z.string().min(1, 'City is required'),
-//   zip: z.string().min(1, 'Zip code is required'),
-//   country: z.string().min(1, 'Country is required'),
-//   continent: z.string().min(1, 'Continent is required'),
-//   lat: z.number().min(-90).max(90),
-//   lng: z.number().min(-180).max(180),
-// });
+const locationSchema = z.object({
+  address: z.string().optional(),
+  city: z.string().optional(),
+  zip: z.string().optional(),
+  country: z.string().optional(),
+});
 
 const metaSchema = z.object({
   wifi: z.boolean(),
@@ -36,6 +22,7 @@ export const addVenueSchema = z.object({
   price: z.number().min(1, 'Price must be at least 1'),
   maxGuests: z.number().min(1, 'Must have at least 1 maximum guest'),
   meta: metaSchema,
+  location: locationSchema.optional(),
 });
 
 export type CreateVenueDTO = z.infer<typeof addVenueSchema>;
