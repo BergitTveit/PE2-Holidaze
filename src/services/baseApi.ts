f45b80b04cf3588fbc26640a344f8caa67113eff
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-08 22:17:09 +0100

    Create baseApi and authApi with redux toolkit

---
+++ src/services/baseApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store/store';
+
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      headers.set('X-Noroff-API-Key', import.meta.env.VITE_API_KEY);
+
+      const token = (getState() as RootState).auth.accessToken;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['Venue', 'Profile', 'Booking'],
+  endpoints: () => ({}),
+});
