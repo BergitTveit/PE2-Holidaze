3cde556f3f204a70eff1b1080f1ca649e28f3896
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-08 22:22:01 +0100

    Refactor LogoutButton to keep in line with new RTK query logic

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,9 +1,18 @@
-import { useLogout } from '../../hooks/useLogout';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../../hooks/useStore';
 import Button from '../common/Buttons';
+import { logout } from '../../store/slices/authSlice';
+import { persistor } from '../../store/store';
 
 const LogoutButton = () => {
-  const handleLogout = useLogout();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    dispatch(logout());
+    await persistor.purge();
+    navigate('/');
+  };
   return (
     <Button onClick={handleLogout} className="bg-red-500 hover:bg-red-600 text-white">
       Logout
