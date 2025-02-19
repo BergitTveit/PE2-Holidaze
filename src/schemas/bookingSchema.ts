b6e083d153db6337556ee69059ca5744b9b5b034
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-19 15:18:03 +0100

    add zod validation for max guests

--- src/schemas/bookingSchema.ts
+++ src/schemas/bookingSchema.ts
@@ -1,10 +1,10 @@
 import * as z from 'zod';
 
-export const bookingSchema = z.object({
+export const bookingSchema = (maxGuests: number) =>z.object({
   dateFrom: z.string(),
   dateTo: z.string(),
-  guests: z.number().int().min(1, 'Must have at least 1 guest'),
+  guests: z.number().int().min(1, 'Must have at least 1 guest').max(maxGuests, `Cannot exceed ${maxGuests} guests`),
   venueId: z.string(),
 });
 
-export type BookingFormData = z.infer<typeof bookingSchema>;
+export type BookingFormData = z.infer<ReturnType<typeof bookingSchema>>;
