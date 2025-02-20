8d9f8835b137d1e99bffbd3448ad34d7490860fc
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-20 12:18:59 +0100

    Add errorhandling to CreateVenuePage

--- src/pages/CreateVenuePage.tsx
+++ src/pages/CreateVenuePage.tsx
@@ -1,17 +1,23 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useGetVenueByIdQuery } from '../services/venuesApi';
-import { AddVenueForm } from '../components/venues/forms/addVenueForm';
+import { AddVenueForm } from '../components/venues/forms/AddVenueForm';
 import { Loader } from 'lucide-react';
 
 const CreateVenuePage = () => {
   const { id } = useParams();
   const isEditMode = Boolean(id);
-  const { data: venue, isLoading } = useGetVenueByIdQuery(id!, {
+
+  const {
+    data: venue,
+    isLoading,
+    error,
+  } = useGetVenueByIdQuery(id!, {
     skip: !isEditMode,
   });
   const navigate = useNavigate();
 
   if (isEditMode && isLoading) return <Loader />;
+  if (isEditMode && error) return <div>Error loading venue</div>;
   if (isEditMode && !venue) return <div>Venue not found</div>;
 
   return (
