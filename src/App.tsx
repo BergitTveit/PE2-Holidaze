1f1dd89c5ad837efcb588aa0cf92069937cc496e
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-21 10:04:44 +0100

    Add hotjar for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import { AuthWrapper } from './components/auth/AuthWrapper';
+import useHotjar from './hooks/useHotjar';
 
 export default function App() {
+  useHotjar(5313147);
   return (
     <BrowserRouter>
       <AuthWrapper>
