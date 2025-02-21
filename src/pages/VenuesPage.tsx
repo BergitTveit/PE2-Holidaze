4928041e1b5e4097600c69c186ccdebb5d25b6c8
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-21 17:59:02 +0100

    Addstylingto venues page and implement heading component

--- src/pages/VenuesPage.tsx
+++ src/pages/VenuesPage.tsx
@@ -4,6 +4,8 @@ import { VenueGrid } from '../components/venues/VenueGrid';
 import { SearchBar } from '../components/common/searchBar/SearchBar';
 import { Pagination } from '../components/common/pagination';
 import { MessageDisplay } from '../components/common/feedback/MessageDisplay';
+import { Heading } from '../components/common/heading';
+
 
 const VenuesPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -59,29 +61,34 @@ const VenuesPage = () => {
   }
 
   return (
-    <div>
-      <SearchBar />
-      {venues?.data?.length === 0 ? (
-        <MessageDisplay
-          title="No venues found"
-          message="Try adjusting your search or location filters"
-          variant="empty"
-        />
-      ) : (
-        <>
-          <VenueGrid venues={venues?.data || []} />
-          {venues?.meta && (
-            <Pagination
-              currentPage={currentPage}
-              pageCount={venues.meta.pageCount}
-              isLoading={isLoadingMore}
-              onPageChange={handlePageChange}
-              isFirstPage={venues.meta.isFirstPage}
-              isLastPage={venues.meta.isLastPage}
-            />
-          )}
-        </>
-      )}
+    <div className="relative bg-neutral min-h-screen flex justify-center px-4 py-0 sm:px-8">
+      {' '}
+      <div className="w-full max-w-screen-lg bg-white shadow-xl min-h-screen p-6 relative z-10">
+        <SearchBar />
+        {venues?.data?.length === 0 ? (
+          <MessageDisplay
+            title="No venues found"
+            message="Try adjusting your search or location filters"
+            variant="empty"
+          />
+        ) : (
+          <>
+            <Heading />
+            <VenueGrid venues={venues?.data || []} />
+
+            {venues?.meta && (
+              <Pagination
+                currentPage={currentPage}
+                pageCount={venues.meta.pageCount}
+                isLoading={isLoadingMore}
+                onPageChange={handlePageChange}
+                isFirstPage={venues.meta.isFirstPage}
+                isLastPage={venues.meta.isLastPage}
+              />
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
