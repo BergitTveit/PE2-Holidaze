d24525e9e5e2bdf6ad772dba6d1f490c23f59c1a
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-21 17:55:30 +0100

    Add heading component

---
+++ src/components/common/Heading.tsx
@@ -0,0 +1,9 @@
+export const Heading = () => {
+  return (
+    <div className="flex items-center gap-4 mb-8 mt-8">
+      <div className="h-px bg-primary-dark flex-grow"></div>
+      <h2 className="text-xl font-medium px-4  bg-white">Location</h2>
+      <div className="h-px bg-primary-dark flex-grow"></div>
+    </div>
+  );
+};
