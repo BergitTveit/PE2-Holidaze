7674ec8ccb70d49e88ec651c8b795c744dba61e9
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-22 19:45:17 +0100

    Add styling to layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,10 +4,13 @@ import { Footer } from './Footer';
 
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-neutral">
       <Header />
-      <main className="flex-1">
-        <Outlet />
+      {/* White container for main content */}
+      <main className="flex-1 flex justify-center px-4 sm:px-8">
+        <div className="w-full max-w-screen-lg bg-white shadow-xl p-6">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
