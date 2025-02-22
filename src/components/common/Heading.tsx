a05f4f97cf29df835e05fb1923dbcd36da85fd85
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-22 20:32:52 +0100

    Heading for venues

--- src/components/common/Heading.tsx
+++ src/components/common/Heading.tsx
@@ -1,8 +1,8 @@
 export const Heading = () => {
   return (
-    <div className="flex items-center gap-4 mb-8 mt-8">
+    <div className="flex items-center py-8 ">
       <div className="h-px bg-primary-dark flex-grow"></div>
-      <h2 className="text-xl font-medium px-4  bg-white">Location</h2>
+      <h2 className="text-xl font-medium px-4  bg-white">Venues</h2>
       <div className="h-px bg-primary-dark flex-grow"></div>
     </div>
   );
