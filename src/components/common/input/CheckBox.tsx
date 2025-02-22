25691a94d3cd10007069f5a38b4bc21b81fd3647
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2025-02-22 20:23:33 +0100

    style checkbox input

--- src/components/common/input/CheckBox.tsx
+++ src/components/common/input/CheckBox.tsx
@@ -11,8 +11,13 @@ export const CheckboxInput = <T extends FieldValues>({
 }: CheckboxInputProps<T>) => {
   return (
     <div className={className}>
-      <input {...register(name)} type="checkbox" id={name} className="h-4 w-4 border-gray-300 " />
-      <label htmlFor={name} className="ml-2 block text-sm">
+      <input
+        {...register(name)}
+        type="checkbox"
+        id={name}
+        className="h-4 w-4 border-neutral text-primary-dark focus:ring-primary-dark/20 accent-primary-dark"
+      />
+      <label htmlFor={name} className="ml-2  block text-sm">
         {label}
       </label>
     </div>
